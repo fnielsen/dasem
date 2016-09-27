6e6061ea6514e20b5e65c6191febe45bfc01c3c2
fnielsen <fn@imm.dtu.dk>
2016-09-27 16:31:17 +0200

    Create __main__.py as a frame

---
+++ dasem/__main__.py
@@ -0,0 +1,17 @@
+"""
+Usage:
+   dasem <query>
+
+Options:
+  --max-n-pages=<int>  Maximum number of pages
+  -v --verbose         Verbose debug messaging
+
+"""
+
+
+def main():
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    main()
