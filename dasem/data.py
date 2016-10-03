06be0787c1f3d0be6b750648ff788fe15c6e1517
fnielsen <fn@imm.dtu.dk>
2016-10-03 18:50:26 +0200

    Add parameter include_problems

--- dasem/data.py
+++ dasem/data.py
@@ -5,8 +5,14 @@ from os.path import join, split
 from pandas import read_csv
 
 
-def wordsim353():
+def wordsim353(include_problems=False):
     """Read and return wordsim353 dataset.
+
+    Parameters
+    ----------
+    include_problems : bool, optional
+        Indicator for whether rows with problematic translations
+        between Danish and English should be returned [default: False].
 
     Returns
     -------
@@ -15,7 +21,7 @@ def wordsim353():
 
     Examples
     --------
-    >>> df = wordsim353()
+    >>> df = wordsim353(include_problems=True)
     >>> df.shape
     (353, 6)
 
@@ -23,4 +29,8 @@ def wordsim353():
     filename = join(split(__file__)[0], 'data', 'wordsim353-da',
                     'combined.csv')
     df = read_csv(filename)
+
+    if include_problems:
+        df = df[df.Problem != 1]
+
     return df
