34046e290842108212d71f6cf2445d7015bf2423
fnielsen <fn@imm.dtu.dk>
2016-10-04 22:42:58 +0200

    Create text.py

---
+++ dasem/text.py
@@ -0,0 +1,35 @@
+"""text."""
+
+
+from nltk import sent_tokenize, word_tokenize
+
+
+def sentence_tokenize(text):
+    """Tokenize a Danish text into sentence.
+
+    The model from NTLK trained on Danish is used.
+
+    Parameters
+    ----------
+    text : str
+        The text to be tokenized.
+
+    Returns
+    -------
+    sentences : list of str
+        Sentences as list of strings.
+
+    Examples
+    --------
+    >>> text = 'Hvad!? Hvor har du f.eks. siddet?'
+    >>> sentences = sentence_tokenize(text)
+    >>> sentences
+    ['Hvad!?', 'Hvor har du f.eks. siddet?']
+
+    """
+    return sent_tokenize(text, language='danish')
+
+
+def word_tokenize(sentence):
+    """Tokenize a Danish sentence into words."""
+    return word_tokenize(sentence)
