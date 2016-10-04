36c95f09fcd8dd5ead96a9b7827cd5dbb7ffb3d4
fnielsen <fn@imm.dtu.dk>
2016-10-04 17:38:17 +0200

    Add functions for reading

--- dasem/data.py
+++ dasem/data.py
@@ -1,8 +1,47 @@
-"""Data."""
+"""Data.
+
+Functions to read datasets from the data subdirectory.
+
+"""
 
 from os.path import join, split
 
 from pandas import read_csv
+
+
+def four_words():
+    """Read and return four words odd-one-out dataset.
+
+    Returns
+    -------
+    >>> df = four_words()
+    >>> df.ix[0, 'word4'] == 'stol'
+    True
+
+    """
+    filename = join(split(__file__)[0], 'data', 'four_words.csv')
+    df = read_csv(filename, encoding='utf-8')
+    return df
+
+
+def verbal_analogies():
+    """Read and return verbal analogies dataset.
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        Dataframe with verbal analogies.
+
+    Examples
+    --------
+    >>> df = verbal_analogies()
+    >>> df.ix[0, :].tolist() == ['mand', 'kvinde', 'dreng', 'pige']
+    True
+
+    """
+    filename = join(split(__file__)[0], 'data', 'verbal_analogies.csv')
+    df = read_csv(filename, encoding='utf-8')
+    return df
 
 
 def wordsim353(include_problems=False):
@@ -25,12 +64,17 @@ def wordsim353(include_problems=False):
     >>> df.shape
     (353, 6)
 
+    References
+    ----------
+    The WordSimilarity-353 Test Collection,
+    http://www.cs.technion.ac.il/~gabr/resources/data/wordsim353/
+
     """
     filename = join(split(__file__)[0], 'data', 'wordsim353-da',
                     'combined.csv')
     df = read_csv(filename)
 
-    if include_problems:
+    if not include_problems:
         df = df[df.Problem != 1]
 
     return df
