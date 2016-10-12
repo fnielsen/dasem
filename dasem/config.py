d1e5f9c19841f506a89b60735992f44dca9e373c
fnielsen <fn@imm.dtu.dk>
2016-10-12 21:21:42 +0200

    Fix docstring style

--- dasem/config.py
+++ dasem/config.py
@@ -1,8 +1,16 @@
-"""config"""
+"""config."""
 
 
 from os.path import expanduser, join
 
 
 def data_directory():
+    """Return path for the data directory.
+
+    Returns
+    -------
+    dirname : str
+        Pathname for the data directory.
+
+    """
     return join(expanduser('~'), 'dasem_data')
