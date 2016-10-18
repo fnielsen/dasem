e3b690285ef09a02447d2aa4668cc0e8fb60d70a
fnielsen <fn@imm.dtu.dk>
2016-10-18 19:49:29 +0200

    Add semantic object

--- dasem/app/__init__.py
+++ dasem/app/__init__.py
@@ -7,11 +7,13 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 
 from ..dannet import Dannet
+from ..semantic import Semantic
 
 
 app = Flask(__name__)
 Bootstrap(app)
 
-app._dasem_dannet = Dannet()
+app.dasem_dannet = Dannet()
+app.dasem_semantic = Semantic()
 
 from . import views
