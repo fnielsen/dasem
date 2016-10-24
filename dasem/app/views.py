4e295241b99947c6da48b405caf7b4911fb8e8d5
fnielsen <fn@imm.dtu.dk>
2016-10-25 00:18:52 +0200

    Add ePAROLE lemmatizing

--- dasem/app/views.py
+++ dasem/app/views.py
@@ -9,7 +9,7 @@ from . import app
 
 
 DANNET_SYNSET_QUERY = u"""
-select w.form, ws.word_id, s.label, ws.synset_id, w.pos, s.gloss 
+select w.form, ws.word_id, s.label, ws.synset_id, w.pos, s.gloss
 from words w, wordsenses ws, synsets s
 where w.form = '{word}'
   and ws.word_id = w.word_id
@@ -41,7 +41,7 @@ def index():
         # https://github.com/yhat/db.py/issues/90
         # df = app._dasem_dannet.db.query(
         #     query, data={'word': first_word})
-        
+
         query = DANNET_SYNSET_QUERY.format(word=word)
         dannet_synsets_table = app.dasem_dannet.db.query(query).to_html()
 
@@ -54,10 +54,15 @@ def index():
         # Word not in vocabulary
         w2v_similar = []
     esa_related = app.dasem_wikipedia_esa.related(q)
-    
+
+    eparole_lemmas = {}
+    for word in words:
+        eparole_lemmas[word] = app.dasem_eparole.word_to_lemmas(word)
+
     return render_template(
         'index.html', q=q,
         dannet_synset_table=dannet_synsets_table,
         dannet_relations_table=dannet_relations_table,
         w2v_similar=w2v_similar,
-        esa_related=esa_related)
+        esa_related=esa_related,
+        eparole_lemmas=eparole_lemmas)
