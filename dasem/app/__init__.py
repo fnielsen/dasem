d04ec12a021857666c38b07503a8a9b261321bba
fnielsen <fn@imm.dtu.dk>
2016-10-25 00:18:19 +0200

    Add logging ePAROLE handling

--- dasem/app/__init__.py
+++ dasem/app/__init__.py
@@ -3,18 +3,29 @@
 
 from __future__ import absolute_import, division, print_function
 
+import logging
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
 from ..dannet import Dannet
+from ..eparole import EParole
 from ..wikipedia import ExplicitSemanticAnalysis, Word2Vec
 
 
 app = Flask(__name__)
 Bootstrap(app)
 
-app.dasem_dannet = Dannet()
-app.dasem_wikipedia_esa = ExplicitSemanticAnalysis(display=True)
-app.dasem_wikipedia_w2v = Word2Vec()
+logging_level = logging.DEBUG
+if not app.debug:
+    app.logger.setLevel(logging_level)
+    logging.basicConfig()
+
+app.logger.info('Setting up datasets')
+app.dasem_dannet = Dannet(logging_level=logging_level)
+app.dasem_wikipedia_esa = ExplicitSemanticAnalysis(logging_level=logging_level)
+app.dasem_wikipedia_w2v = Word2Vec(logging_level=logging_level)
+app.dasem_eparole = EParole(logging_level=logging_level)
+app.logger.info('Datasets loaded')
 
 from . import views
