3be3ce5446e5df466260f41020925b27b0f91690
fnielsen <fn@imm.dtu.dk>
2017-02-13 22:18:53 +0100

    Implement decompounder

--- dasem/text.py
+++ dasem/text.py
@@ -1,10 +1,85 @@
-"""text."""
+"""text.
 
+Usage:
+  text decompound <text>
 
-from nltk import sent_tokenize
+"""
+
+from codecs import open
+
+from os.path import join, split
+
+from nltk import sent_tokenize, WordPunctTokenizer
 from nltk import word_tokenize as nltk_word_tokenize
 
 
+class Decompounder(object):
+    """Word decompunder."""
+    
+    def __init__(self):
+        """Setup map."""
+        self.word_tokenizer = WordPunctTokenizer()
+        
+        filename = join(split(__file__)[0], 'data', 'compounds.txt')
+
+        self.decompound_map = {}
+        with open(filename, encoding='utf-8') as fid:
+            for line in fid:
+                parts = line.strip().split('|')
+                compound = "".join(parts)
+                decompounded_parts = [part for part in parts if part != 's']
+                decompounded = " ".join(decompounded_parts)
+                self.decompound_map[compound] = decompounded
+
+    def decompound_text(self, text):
+        """Return decompounded text.
+
+        Parameters
+        ----------
+        text : str
+            Text as a (unicode) str.
+
+        Returns
+        -------
+        decompounded : str
+            String with decompounded parts separated by a whitespace.
+
+        Examples
+        --------
+        >>> decompounder = Decompounder()
+        >>> text = 'Det er en investeringsvirksomhed.'
+        >>> decomp = decompounder.decompound_word(text)
+        >>> decomp == 'det er en investering virksomhed'
+        True
+
+        """
+        tokens = self.word_tokenizer.tokenize(text)
+        return " ".join(self.decompound_word(token.lower()) for token in tokens)
+        
+    def decompound_word(self, word):
+        """Return decompounded word.
+
+        Parameters
+        ----------
+        word : str
+            Word as a (unicode) str.
+
+        Returns
+        -------
+        decompounded : str
+            String with decompounded parts separated by a whitespace.
+
+        Examples
+        --------
+        >>> decompounder = Decompounder()
+        >>> decomp = decompounder.decompound_word('investeringsvirksomhed')
+        >>> decomp == 'investering virksomhed'
+        True
+
+        """
+        return self.decompound_map.get(word, word)
+
+                
 def sentence_tokenize(text):
     """Tokenize a Danish text into sentence.
 
@@ -34,3 +109,29 @@ def sentence_tokenize(text):
 def word_tokenize(sentence):
     """Tokenize a Danish sentence into words."""
     return nltk_word_tokenize(sentence)
+
+
+def main():
+    """Handle command-line interface."""
+    from docopt import docopt
+
+    arguments = docopt(__doc__)
+
+    if arguments['decompound']:
+        text = arguments['<text>']
+        decompounder = Decompounder()
+        decompounded = decompounder.decompound_text(text)
+        print(decompounded)
+        
+    elif arguments['decompound-word']:
+        word = arguments['<word>']
+        decompounder = Decompounder()
+        decompounded = decompounder.decompound_word(word)
+        print(decompounded)
+
+    else:
+        assert False
+        
+
+if __name__ == '__main__':
+    main()
