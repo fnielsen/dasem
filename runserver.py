ca33afb60d98da4d54c1bce8eef6e7251aaaff7f
fnielsen <fn@imm.dtu.dk>
2017-02-22 17:50:52 +0100

    Change to use app factory

--- runserver.py
+++ runserver.py
@@ -1,7 +1,15 @@
-from dasem.app import app
+"""Entrypoint to start app."""
+
+from gevent.wsgi import WSGIServer
+
+import logging
+
+from dasem.app import create_app
+
+
+app = create_app(logging_level=logging.DEBUG)
 
 # WSGIServer server better than werkzeug
 # http://stackoverflow.com/questions/37962925/
-from gevent.wsgi import WSGIServer
 http_server = WSGIServer(('', 5000), app)
 http_server.serve_forever()
