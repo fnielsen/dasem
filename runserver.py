b3c9fb3c21ffd36418ee479eebb5bb9bc268c356
fnielsen <fn@imm.dtu.dk>
2017-02-23 17:35:11 +0100

    Change logging

--- runserver.py
+++ runserver.py
@@ -7,9 +7,22 @@ import logging
 from dasem.app import create_app
 
 
-app = create_app(logging_level=logging.DEBUG)
+logging_level = logging.DEBUG
+
+logger = logging.getLogger()
+logger.setLevel(logging_level)
+logging_handler = logging.StreamHandler()
+logging_handler.setLevel(logging_level)
+logging_formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging_handler.setFormatter(logging_formatter)
+logger.addHandler(logging_handler)
+logger.info('Logging setup')
+
+logger.info('Creating app')
+app = create_app()
 
 # WSGIServer server better than werkzeug
 # http://stackoverflow.com/questions/37962925/
-http_server = WSGIServer(('', 5000), app)
+http_server = WSGIServer(('', 5000), app, log=logger)
 http_server.serve_forever()
