ad0ed0b60db6b527b0c210d2e1a23d529d36889d
fnielsen <fn@imm.dtu.dk>
2017-03-17 21:42:40 +0100

    Create test_gutenberg.py

---
+++ tests/test_gutenberg.py
@@ -0,0 +1,14 @@
+
+import pytest
+
+from dasem.gutenberg import Word2Vec
+
+
+@pytest.fixture
+def w2v():
+    return Word2Vec()
+
+
+def test_w2v(w2v):
+    word_and_similarities = w2v.most_similar('dreng')
+    assert len(word_and_similarities) == 10
